213ac08e9fee77656c651ef8121a653af24ed811
Yegiazaryan Tatevik <barev.tatevik@gmail.com>
2019-09-17 03:41:49 +0400

    First page is done, but database

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-main.js
+jQuery(document).ready(function($) {
+	$('#modalPrint').fadeOut();
+    
+    $('ul.buttons > li>a.print').on('click', function(event) {
+    	event.preventDefault();
+	    $('#modalPrint').fadeIn(800);
+    });
+
+    $('#modalPrint').on('click',function(event) {
+    	event.preventDefault();
+    	if (event.target.id == 'modalPrint') {
+    		$(this).fadeOut(500);
+    	}
+
+    });
+
+    $('#mainTable table > tbody > tr').on('click', function(event) {
+    	event.preventDefault();
+    	$('#mainTable table > tbody > tr').not(this).removeClass('active');
+    	$(this).toggleClass('active');
+    });
+
+
+});
