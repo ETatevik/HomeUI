513fa1265819c75add9b7ab2f921ae52c055a72e
Yegiazaryan Tatevik <barev.tatevik@gmail.com>
2019-09-19 04:30:29 +0400

    Post And Success : Done!

--- js/main.js
+++ js/main.js
@@ -19,5 +19,25 @@ jQuery(document).ready(function($) {
     	$(this).toggleClass('active');
     });
 
+    $('#crEdPost label[for="langu"]').on('click', function(event) {
+        event.preventDefault();
+         console.log(event.target.tagName.toLowerCase())
+        if ( (event.target.tagName.toLowerCase() == 'li' && event.target.className == "activeLang") 
+            || event.target.tagName.toLowerCase() == "label" 
+            || $(this).parent('li.activeLang')) {
+           
+            let li = "";
+            if ($(this).hasClass('activeLang')){
+                li = $(this);
+            }else if(event.target.tagName == "img"){
+                li = $('#crEdPost label[for="langu"] ul li.activeLang');
+            } else if( $(this).attr('for') == "langu"){
+                li = $(this).children("ul").children('.activeLang');
+            }
+            $('#langu li').not(li).fadeToggle(200);   
+
+        }
+    });
+
 
 });
