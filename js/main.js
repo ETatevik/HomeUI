eaefe7d4fea1b7899b4a400667fdf0f7e1b3dfa2
Yegiazaryan Tatevik <barev.tatevik@gmail.com>
2019-09-22 00:51:15 +0400

    Debuging Setp 3: files problem

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+(function($) {
+  $.fn.inputFilter = function(inputFilter) {
+    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      }
+    });
+  };
+}(jQuery));
+
+
 jQuery(document).ready(function($) {
     
     $('ul.buttons > li>a.print').on('click', function(event) {
@@ -19,24 +35,64 @@ jQuery(document).ready(function($) {
     	$(this).toggleClass('active');
     });
 
-    $('#crEdPost label[for="langu"]').on('click', function(event) {
-        event.preventDefault();
-         console.log(event.target.tagName.toLowerCase())
-        if ( (event.target.tagName.toLowerCase() == 'li' && event.target.className == "activeLang") 
-            || event.target.tagName.toLowerCase() == "label" 
-            || $(this).parent('li.activeLang')) {
-           
-            let li = "";
-            if ($(this).hasClass('activeLang')){
-                li = $(this);
-            }else if(event.target.tagName == "img"){
-                li = $('#crEdPost label[for="langu"] ul li.activeLang');
-            } else if( $(this).attr('for') == "langu"){
-                li = $(this).children("ul").children('.activeLang');
-            }
-            $('#langu li').not(li).fadeToggle(200);   
-
+    $.fn.chackLang = function(textLang){
+        let glText = '';
+        if(textLang == "English"){
+            glText = "eng";
+        }else if(textLang == "Armenian"){
+            glText = "arm";
+        }else if(textLang == "Russian"){
+            glText = "ru";
         }
+        return $(this).val(glText);
+    }
+
+    $('input#lang').chackLang($('#langu li.activeLang > span').text());
+    $('#langu li').on('click', function(event) {
+        event.preventDefault();
+        
+        if (!$(this).hasClass('activeLang')) {
+            $('#langu li.activeLang').css('order', $(this).css('order'));
+            $(this).css('order', 1);
+            $('#langu li').removeClass('activeLang');
+            $(this).addClass('activeLang');
+            $('#langu li').not(this).fadeOut(200); 
+        }else{
+            $('#langu li').not(this).fadeToggle(200); 
+        }
+        $('input#lang').chackLang($(this).children('span').text());
+    });
+
+
+    $('input#iditi').inputFilter(function(value) {
+        return /^\d*$/.test(value);
+    });
+
+
+    $("#addMultiFile").on("change", function() {
+        if ($(this).prop('files').length > 2) {
+            alert("You can select only 2 file : 1 image and 1 audio");
+            $(this).val('');
+        }else if($(this).prop('files').length == 2 && (($(this).prop('files')[0].type == "image/png" && $(this).prop('files')[1].type ==  "image/png")
+            || ($(this).prop('files')[0].type == "image/jpg" && $(this).prop('files')[1].type ==  "image/jpg")
+            || ($(this).prop('files')[0].type == "image/jpg" && $(this).prop('files')[1].type ==  "image/png")
+            || ($(this).prop('files')[0].type == "image/png" && $(this).prop('files')[1].type ==  "image/jpg"))){
+            alert("You can select only  1 image");
+            $(this).val('');
+        }else if($(this).prop('files').length == 1 
+            && ($(this).prop('files')[0].type == "image/png" ||  $(this).prop('files')[0].type == "image/jpg") 
+            && $(this).prop('files')[0].size / 1048576 <= 5){
+            // $('input[name="image"]').eq(0).prop('files') = $(this).prop('files')[0];
+            console.log($(this).prop('files')[0])
+        }
+
+        // {
+        //     let fileSize = $(this).prop('files')[0].size;
+        //     fileSize = fileSize / 1048576; //size in mb
+        //     console.log(fileSize)
+        // } 
+        console.log($('input[name="image"]'))
+
     });
 
 
