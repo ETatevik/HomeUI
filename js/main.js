66fc3da53f2ff0bad64ea66407d6cba793eca908
Yegiazaryan Tatevik <barev.tatevik@gmail.com>
2019-09-23 02:36:24 +0400

    Debuging Setp 4: Q.base64, and newPage: P.blur

--- js/main.js
+++ js/main.js
@@ -11,31 +11,8 @@
       }
     });
   };
-}(jQuery));
 
-
-jQuery(document).ready(function($) {
-    
-    $('ul.buttons > li>a.print').on('click', function(event) {
-    	event.preventDefault();
-	    $('#modalPrint').addClass('anime');
-    });
-
-    $('#modalPrint').on('click',function(event) {
-    	event.preventDefault();
-    	if (event.target.id == 'modalPrint') {
-    		$(this).removeClass('anime');
-    	}
-
-    });
-
-    $('#mainTable table > tbody > tr').on('click', function(event) {
-    	event.preventDefault();
-    	$('#mainTable table > tbody > tr').not(this).removeClass('active');
-    	$(this).toggleClass('active');
-    });
-
-    $.fn.chackLang = function(textLang){
+   $.fn.chackLang = function(textLang){
         let glText = '';
         if(textLang == "English"){
             glText = "eng";
@@ -46,54 +23,147 @@ jQuery(document).ready(function($) {
         }
         return $(this).val(glText);
     }
+}(jQuery));
 
-    $('input#lang').chackLang($('#langu li.activeLang > span').text());
-    $('#langu li').on('click', function(event) {
-        event.preventDefault();
-        
-        if (!$(this).hasClass('activeLang')) {
-            $('#langu li.activeLang').css('order', $(this).css('order'));
-            $(this).css('order', 1);
-            $('#langu li').removeClass('activeLang');
-            $(this).addClass('activeLang');
-            $('#langu li').not(this).fadeOut(200); 
-        }else{
-            $('#langu li').not(this).fadeToggle(200); 
+
+jQuery(document).ready(function($) {
+
+    // homeUI html script
+    {
+        // Print modal window
+        $('ul.buttons > li>a.print').on('click', function(event) {
+            event.preventDefault();
+            $('#modalPrint').addClass('anime');
+        });
+
+        $('#modalPrint').on('click',function(event) {
+            event.preventDefault();
+            if (event.target.id == 'modalPrint') {
+                $(this).removeClass('anime');
+            }
+
+        });
+
+        // table body choose event
+        $('#mainTable table > tbody > tr').on('click', function(event) {
+            event.preventDefault();
+            $('#mainTable table > tbody > tr').not(this).removeClass('active');
+            $(this).toggleClass('active');
+        });
+    }
+
+
+    // addPost html script
+    {
+        //  choose language and save that in input
+        $('input#lang').chackLang($('#langu li.activeLang > span').text());
+        $('#langu li').on('click', function(event) {
+            event.preventDefault();
+            
+            if (!$(this).hasClass('activeLang')) {
+                $('#langu li.activeLang').css('order', $(this).css('order'));
+                $(this).css('order', 1);
+                $('#langu li').removeClass('activeLang');
+                $(this).addClass('activeLang');
+                $('#langu li').not(this).fadeOut(200); 
+            }else{
+                $('#langu li').not(this).fadeToggle(200); 
+            }
+            $('input#lang').chackLang($(this).children('span').text());
+        });
+
+        // id only number
+        $('input#iditi').inputFilter(function(value) {
+            return /^\d*$/.test(value);
+        });
+
+        // choose image and audio file save that in input and make base 64 texts
+        {
+            let locText = '';
+            var handleFileSelect = function(evt) {
+                var files = evt.target.files;
+                var file = files[0];
+                if (files && file) {  
+                    var reader = new FileReader();
+
+                    reader.onload = function(readerEvt) {
+                        var binaryString = readerEvt.target.result;
+                        console.log(btoa(binaryString));
+                    };
+
+                    reader.readAsBinaryString(file);
+                }
+                   
+            };
+
+            $(".addMultiFile > input[type='file']").on("change", function(event) {
+                
+                if ($(this).prop('files')[0].size / 1048576 > 5){
+                    alert('Maximum file size 5 MB');
+                    $(this).val('');
+                }else{
+                    $(this).attr('disabled', 'true');
+                    $(this).removeClass('active');
+                    $(".addMultiFile > input[type='file']").not(this).removeAttr('disabled')
+                    $(".addMultiFile > input[type='file']").not(this).addClass('active');
+
+                    if ($(this).attr('name') == 'image') {
+                        if(locText.length  == 0){
+                            locText = $(this).prop('files')[0].name;
+                        }else if(/  &  /.test(locText)){
+                            locText = locText.split('  &  ');
+                            locText.shift();
+                            locText = locText.join('');
+                            locText += '  &  ' + $(this).prop('files')[0].name;
+                        }
+                        handleFileSelect(event);
+                    }else{
+                        if(!/  &  /.test(locText)){
+                            locText +=  '  &  ' + $(this).prop('files')[0].name;
+                        }else{
+                            locText = locText.split('  &  ');
+                            locText.shift();
+                            locText = locText.join('');
+                            locText += '  &  ' + $(this).prop('files')[0].name;
+                        }
+                        handleFileSelect(event);
+
+                    }
+
+                    $('.addAudioImgNames').text(locText);
+
+                }
+            });
         }
-        $('input#lang').chackLang($(this).children('span').text());
-    });
 
 
-    $('input#iditi').inputFilter(function(value) {
-        return /^\d*$/.test(value);
-    });
+        // post form submit in server
+        $('form[name="postsMU"]').submit(function(event) {
+            $('input').removeAttr('disabled');
+        });
+    }
 
-
-    $("#addMultiFile").on("change", function() {
-        if ($(this).prop('files').length > 2) {
-            alert("You can select only 2 file : 1 image and 1 audio");
-            $(this).val('');
-        }else if($(this).prop('files').length == 2 && (($(this).prop('files')[0].type == "image/png" && $(this).prop('files')[1].type ==  "image/png")
-            || ($(this).prop('files')[0].type == "image/jpg" && $(this).prop('files')[1].type ==  "image/jpg")
-            || ($(this).prop('files')[0].type == "image/jpg" && $(this).prop('files')[1].type ==  "image/png")
-            || ($(this).prop('files')[0].type == "image/png" && $(this).prop('files')[1].type ==  "image/jpg"))){
-            alert("You can select only  1 image");
-            $(this).val('');
-        }else if($(this).prop('files').length == 1 
-            && ($(this).prop('files')[0].type == "image/png" ||  $(this).prop('files')[0].type == "image/jpg") 
-            && $(this).prop('files')[0].size / 1048576 <= 5){
-            // $('input[name="image"]').eq(0).prop('files') = $(this).prop('files')[0];
-            console.log($(this).prop('files')[0])
-        }
-
-        // {
-        //     let fileSize = $(this).prop('files')[0].size;
-        //     fileSize = fileSize / 1048576; //size in mb
-        //     console.log(fileSize)
-        // } 
-        console.log($('input[name="image"]'))
+    // login (admin) html script // index html
+    {
+        $('#login .col > label').on({
+            click: function(event) {
+                event.preventDefault();
+                $(this).toggleClass('active');
+                $('#login .col > label').not(this).removeClass('active');
+            },
+            blur: function(event){
+                event.preventDefault();
+                $(this).removeClass('active');
+                if($(this).children('input').val() != ""){
+                    $(this).css('borderBottomColor', '#4C73FF');
+                    $(this).children('img').css('opacity', '1');
+                }else{
+                    $(this).css('borderBottomColor', '#CFCFCF');
+                    $(this).children('img').css('opacity', '.25');
+                }
+            }
 
     });
-
+    }
 
 });
